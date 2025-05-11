b73364aadc72ab12f102f655f1d797f17d57c502
Waim908 <waimxj449@foxmail.com>
2025-05-12 00:31:29 +0800

    x11 preferencee test

--- termux-x11/src/main/java/com/termux/x11/LoriePreferences.java
+++ termux-x11/src/main/java/com/termux/x11/LoriePreferences.java
@@ -32,6 +32,7 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.graphics.Color;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
