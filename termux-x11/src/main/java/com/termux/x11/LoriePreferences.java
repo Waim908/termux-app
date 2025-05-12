de4d5722d34d30b0884cf6af1324b4ec0abaa225
Waim908 <160860390+Waim908@users.noreply.github.com>
2025-05-12 19:21:02 +0800

    更新 LoriePreferences.java

--- termux-x11/src/main/java/com/termux/x11/LoriePreferences.java
+++ termux-x11/src/main/java/com/termux/x11/LoriePreferences.java
@@ -155,11 +155,7 @@ public class LoriePreferences extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         loriePreferenceFragment = new LoriePreferenceFragment();
-//        setContentView(R.layout.x11preference);
-        getSupportFragmentManager()
-            .beginTransaction()
-            .replace(android.R.id.content, loriePreferenceFragment)
-            .commit();        
+//        setContentView(R.layout.x11preference);    
         getWindow().getDecorView().setBackgroundColor(Color.parseColor("#888888"));
 //        getSupportFragmentManager().beginTransaction().replace(android.R.id.content, loriePreferenceFragment).commit();
 
