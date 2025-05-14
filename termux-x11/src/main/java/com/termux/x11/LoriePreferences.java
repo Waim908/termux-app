af6670a2d37548cf1fa64384589de6029682377e
Waim908 <waimxj449@foxmail.com>
2025-05-15 02:14:37 +0800

    diasable night theme test  3

--- termux-x11/src/main/java/com/termux/x11/LoriePreferences.java
+++ termux-x11/src/main/java/com/termux/x11/LoriePreferences.java
@@ -152,7 +152,6 @@ public class LoriePreferences extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.AppThemeLight);
         super.onCreate(savedInstanceState);
         loriePreferenceFragment = new LoriePreferenceFragment();
 //        getSupportFragmentManager().beginTransaction().replace(android.R.id.content, loriePreferenceFragment).commit();
@@ -314,7 +313,6 @@ public class LoriePreferences extends AppCompatActivity {
 
         @Override
         public void onCreate(final Bundle savedInstanceState) {
-            setTheme(R.style.AppThemeLight);
             super.onCreate(savedInstanceState);
             SharedPreferences preferences = getPreferenceManager().getSharedPreferences();
 
