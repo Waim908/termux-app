2af88eaad16cf5e408b3e85af57b36788b5a2b16
Waim908 <waimxj449@foxmail.com>
2025-05-15 01:27:49 +0800

    disable x11 preferences night theme test 1

--- termux-x11/src/main/java/com/termux/x11/LoriePreferences.java
+++ termux-x11/src/main/java/com/termux/x11/LoriePreferences.java
@@ -152,6 +152,7 @@ public class LoriePreferences extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Theme.TermuxApp.Light.NoActionBar);
         super.onCreate(savedInstanceState);
         loriePreferenceFragment = new LoriePreferenceFragment();
 //        getSupportFragmentManager().beginTransaction().replace(android.R.id.content, loriePreferenceFragment).commit();
@@ -313,6 +314,7 @@ public class LoriePreferences extends AppCompatActivity {
 
         @Override
         public void onCreate(final Bundle savedInstanceState) {
+            setTheme(R.style.Theme.TermuxApp.Light.NoActionBar);
             super.onCreate(savedInstanceState);
             SharedPreferences preferences = getPreferenceManager().getSharedPreferences();
 
